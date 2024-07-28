2fbb8dde923fba02371add8cda8d209831e1d67d
ovo-Tim <ovo-tim@qq.com>
2024-07-28 21:07:41 +0800

    Finish CommandRunner

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,11 @@
 use serde::{Serialize, Deserialize};
 mod server_runner;
 use server_runner::CommandRunner;
-use tauri::{path::BaseDirectory, Manager};
+use tauri::{async_runtime::block_on, path::BaseDirectory, Manager};
 use std::fs;
 use directories::UserDirs;
+use lazy_static::lazy_static;
+use std::sync::Mutex;
 
 #[derive(Serialize, Deserialize)]
 struct Config {
@@ -32,25 +34,39 @@ static EXTENSION: &str = ".exe";
 #[cfg(not(target_os = "windows"))]
 static EXTENSION: &str = ".bin";
 
-fn start_server(handle: tauri::AppHandle, runner: &mut CommandRunner) -> Result<(), String>{
+lazy_static!{
+    static ref RUNNER:Mutex<CommandRunner> = Mutex::new(CommandRunner::default());
+}
+
+#[tauri::command]
+fn start_server(handle: tauri::AppHandle) -> Result<(), String>{
     let server_path = handle.path().resolve("server/main".to_owned()+EXTENSION, BaseDirectory::Resource);
     if server_path.is_err(){
         return Err("Failed to get server path".into());
     }
     if let Some(_path) = server_path.unwrap().to_str(){
+        let mut runner = RUNNER.lock().unwrap();
         runner.start_server(_path.to_string())?;
+    }else{
+        return Err("Failed to get server path".into());
     }
-
     Ok(())
+}
+
+#[tauri::command]
+fn get_state() -> Result<server_runner::CommandState, String>{
+    if let Ok(mut runner) = RUNNER.lock(){
+        Ok(block_on(runner.get_state())?)
+    }else{
+        Err("Failed to get state. Rust mutex is poisoned.".into())
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let mut runner = CommandRunner::default();
-
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![get_config])
+        .invoke_handler(tauri::generate_handler![get_config, start_server, get_state])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
